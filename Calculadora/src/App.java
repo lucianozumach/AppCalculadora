4104ee24d7d993595310da9457a98d5ea07410d5
lucianozumach <lucianozumach7c@gmail.com>
2025-08-27 15:44:15 -0300

    Update App.java

--- Calculadora/src/App.java
+++ Calculadora/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+
+
+            System.out.println("Soma: " + Calculadora.soma(10, 5));
+            System.out.println("Subtração: " + Calculadora.subtracao(10, 5)); 
+            
+            
+
+            
     }
 }
