fbe1aeb36f4f2a38330910e6ae070389af0b339c
victorhb09 <victor_bona@estudante.sesisenai.org.br>
2025-08-27 15:46:16 -0300

    Teste divisão e multiplicação

--- Calculadora/src/App.java
+++ Calculadora/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        
+        System.out.println("Multiplicação: " + Calculadora.multiplicacao(10, 5));
+
+
+        System.out.println("Divisão: " + Calculadora.divisao(10, 5));
+
+
+
     }
 }
+
+    
+
